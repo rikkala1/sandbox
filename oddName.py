a3d29290e1520652b46362b7350f2d998504e207
jc446932 <venkatreddyrikkala@gmaol.com>
2016-12-09 20:01:11 +1000

    complete the file

--- oddName.py
+++ oddName.py
@@ -1,2 +1,14 @@
 __author__ = 'jc446932'
+valid = False
+while not valid:
+  name = input("Enter your name:")
+  if name !="" or not name.isspace():
+      valid = True
 
+print(valid)
+
+
+
+l = len(name)
+for i in range(0, l, 2):
+    print(name[i])
